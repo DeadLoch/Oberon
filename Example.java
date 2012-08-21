144c4888f4c43d658d6bc8f95dcf2b60fa45a91b
Jaxus <jaxusdev@yahoo.com>
2012-08-21 01:00:48 -0300

    Update Example.java

--- Example.java
+++ Example.java
@@ -4,3 +4,7 @@ public class Example {
 		System.out.println("This is a test Morgan.");
 		}
 	}
+	
+/*I will explain how GitBin works tomorrow, it only takes a little bit of time to get used to.
+Also, I'm off every day except Saturday this week.
+*/
