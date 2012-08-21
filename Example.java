06ec7876578af5310e0bebe9fdca208600f2bf33
Stephen Ussery <jaxusdev@yahoo.com>
2012-08-21 00:01:30 -0400

    Revert "Update Example.java"
    This reverts commit 144c4888f4c43d658d6bc8f95dcf2b60fa45a91b.

--- Example.java
+++ Example.java
@@ -4,7 +4,3 @@ public class Example {
 		System.out.println("This is a test Morgan.");
 		}
 	}
-	
-/*I will explain how GitBin works tomorrow, it only takes a little bit of time to get used to.
-Also, I'm off every day except Saturday this week.
-*/
